611796af072eda411004844549b636f1ac0f005e
sarah <sarah@triggersandsparks.com>
2018-09-05 13:20:27 +0100

    Adjust spacing and balance of post headers.

--- assets/js/containers/intro/index.js
+++ assets/js/containers/intro/index.js
@@ -12,7 +12,11 @@ export default class Intro extends Component {
             <section className="container-fluid template-single">
                 <Header/>
                 <main id="site-content" className="intro">
-                    <ArticleHeader type="intro" title="Oh, hello there. I am Sarah. I make things &amp; I wander a lot. Sometimes I write long, rambling stories." />
+                    <ArticleHeader type="intro" title="Oh, hello there." />
+                    <p>I am Sarah. I make things &amp; I wander a lot. Sometimes I write long, rambling stories.</p>
+                    <h1>Oh, hello there. I am Sarah.</h1>
+                    <h1>I make things &amp; I wander a lot.</h1>
+                    <h1>Sometimes I write long, rambling stories.</h1>
                 </main>
                 <Footer/>
             </section>
